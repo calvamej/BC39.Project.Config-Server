c458e93cbddb731d368cf9fdfb239e7e4ba8acaf
calvamej <cesaralvamejia@gmail.com>
2022-12-01 15:35:28 -0500

    Initial Commit

---
+++ src/test/java/com/bootcamp/project/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.project.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
